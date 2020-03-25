a9bcca70caaab9cdf8d5da66d8a4ae31c970b7e6
Samiylo Kryshu <skryshu@gmail.com>
2020-03-25 14:00:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,43 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  counter = 1
+  array.each { |dwarf|
+    puts "#{counter} #{dwarf}"
+    counter += 1 
+  }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  new_array = []
+  
+  array.each { |element|
+    new_array << "#{element.capitalize}!"
+  }
+  new_array
+  
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  counter = 0
+  array.each do |word|
+    if word.length > 4 
+      counter += 1 
+    end
+    
+  end
+  if counter > 0 
+    return true 
+  else
+    return false 
+  end
+  
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
+   
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.find(array)
+  
+  
 end
